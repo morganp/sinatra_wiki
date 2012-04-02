ff12e42e6b08d614547f66a7d10785afde58be9f
Morgan Prior <morgan_prior@yahoo.co.uk>
2012-04-02 09:58:48 +0200

    commit from gitx seems to have failed to update the code correctly. This is a tidy up commit taking out duplicated code.

--- sinatra_wiki.rb
+++ sinatra_wiki.rb
@@ -20,54 +20,20 @@ configure :development do
   set :cache_enabled, false
 end
 
-before do
-  content_type 'text/html', :charset => 'utf-8'
-  @page = Page.new("home") # Default page
-end
-
-get '/' do
-  @pages = Dir["public/**/*.txt"]
-  #cache erb :home
-  erb :home
-end
-
-get '/:slug' do
-  @page = Page.new(params[:slug])
-  if @page.is_new
-    redirect "/#{@page.name}/edit"
-  else
-    erb :page
-  end
-end
-
-get '/:slug/edit' do
-  auth
-  @page = Page.new(params[:slug])
-  erb :edit
-end
     configure :development do
       # %x(rake expire_cache)
       #set :cache_enabled, false
       set 'analytics', false
-      set 'thing', ['one', 'two']
-      set 'hash', {:value => 'data'}
-
+      
       ActiveRecord::Base.establish_connection(
         :adapter   => 'sqlite3',
         :database  => './db/devel.db'
       )
     end
-
-post '/:slug/edit' do
-  auth
-  nice_title = Slugalizer.slugalize(params[:title])
-  @page = Page.new(nice_title)
-  @page.content = params[:body]
-  #expire_cache "/"
-  #expire_cache "/#{nice_title}"
-  redirect "/#{nice_title}"
+    
     configure :production do
       set 'analytics', true
+      
       db = ENV["DATABASE_URL"]
       if db.match(/postgres:\/\/(.*):(.*)@(.*)\/(.*)/) 
         username = $1
@@ -84,50 +50,49 @@ post '/:slug/edit' do
         )
       end
     end
-    before do
-      content_type 'text/html', :charset => 'utf-8'
-      #@page = Page.new("home") # Default page
+
+  before do
+    content_type 'text/html', :charset => 'utf-8'
+    #@page = Page.find_by_url("home") # Default page
+  end
+
+  get '/' do
+    @pages = Page.all 
+    erb :home
+  end
+
+  get '/:slug' do
+    @page = Page.find_by_url(params[:slug])
+    if @page.nil?
+      redirect "/#{params[:slug]}/edit"
+    else
+      erb :page
     end
+  end
 
-    get '/' do
-      @pages = Page.all 
-      erb :home
-    end
-
-    get '/:slug' do
-      @page = Page.find_by_url(params[:slug])
-      if @page.nil?
-        redirect "/#{params[:slug]}/edit"
-      else
-        erb :page
-      end
-
-    end
-
-    get '/:slug/edit' do
-      auth
-      @page = Page.find_by_url( params[:slug] )
-      if @page.nil?
-        @page = Page.new(
-          :url => params[:slug]
-        )
-      end
-
-      erb :edit
-    end
-
-    post '/:slug/edit' do
-      auth
-      nice_title = Slugalizer.slugalize(params[:title])
-      @page = Page.create(
-       :url  => nice_title,
-       :body => params[:body]
+  get '/:slug/edit' do
+    auth
+    @page = Page.find_by_url( params[:slug] )
+    if @page.nil?
+      @page = Page.new(
+        :url => params[:slug]
       )
-      redirect "/#{nice_title}"
     end
-end
 
-get '/base.css' do
-  #cache sass :base
-  sass :base
-end
+    erb :edit
+  end
+
+  post '/:slug/edit' do
+    auth
+    nice_title = Slugalizer.slugalize(params[:title])
+    @page = Page.create(
+     :url  => nice_title,
+     :body => params[:body]
+    )
+    redirect "/#{nice_title}"
+  end
+
+  get '/base.css' do
+    #cache sass :base
+    sass :base
+  end
