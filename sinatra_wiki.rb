f74335fae8466f0bf794c769c3eb3525cfbfa779
Morgan Prior <morgan@amaras-tech.co.uk>
2012-04-05 23:10:28 +0100

    Modularised The app

--- sinatra_wiki.rb
+++ sinatra_wiki.rb
@@ -8,98 +8,110 @@ Dir["lib/*.rb"].each do |lib|
   require_relative lib
 end
 
-configure do
-  @config = YAML::load(File.read('config.yml')).to_hash.each do |k,v|
-    set k, v
+
+module SinatraWiki
+  VERSION = '0.0.1'
+    
+  ## Load Models
+  Dir[File.dirname(__FILE__) + '/lib/models/*.rb'].each {|file| require file }
+
+
+  class App < Sinatra::Base
+    use Rack::MethodOverride
+    set :public_folder, "public"
+    ## Allow white space control in erb templates using -%>
+    set :erb, :trim => '-'
+
+
+    configure do
+      @config = YAML::load(File.read('config.yml')).to_hash.each do |k,v|
+        set k, v
+      end
+    end
+
+
+    configure :development do
+      set 'analytics', false
+
+      ActiveRecord::Base.establish_connection(
+        :adapter   => 'sqlite3',
+        :pool      => 25,
+        :database  => './db/devel.db'
+      )
+    end
+
+    configure :production do
+      set 'analytics', true
+
+      db = ENV["DATABASE_URL"]
+      if db.match(/postgres:\/\/(.*):(.*)@(.*)\/(.*)/) 
+        username = $1
+        password = $2
+        hostname = $3
+        database = $4
+
+        ActiveRecord::Base.establish_connection(
+          :adapter  => 'postgresql',
+          :host     => hostname,
+          :username => username,
+          :password => password,
+          :database => database
+        )
+      end
+    end
+
+    before do
+      content_type 'text/html', :charset => 'utf-8'
+    end
+
+    get '/' do
+      @pages = Page.all 
+      erb :home
+    end
+
+    get '/base.css' do
+      sass :'stylesheets/base'
+    end
+
+    get '/:slug' do
+      @page = Page.find_by_url(params[:slug])
+      redirect "/#{params[:slug]}/edit" if @page.nil?
+      erb :page
+    end
+
+    get '/:slug/edit' do
+      auth
+      @page = Page.find_by_url( params[:slug] )
+      @new  = @page.nil? 
+      @page = Page.new( :url => params[:slug] ) if @new
+
+      erb :edit
+    end
+
+    post '/:slug/?' do
+      auth
+      nice_title = Slugalizer.slugalize(params[:title])
+      @page      = Page.create(
+        :url  => nice_title,
+        :body => params[:body]
+      )
+      redirect "/" + @page.url
+    end
+
+    put '/:slug/?' do
+      auth
+      nice_title = Slugalizer.slugalize(params[:title])
+      @page      = Page.find_by_url( params[:slug] )
+      @page.url  = nice_title
+      @page.body = params[:body]
+      @page.save
+      redirect "/" + @page.url
+    end
+
   end
 end
-Dir[File.dirname(__FILE__) + '/lib/models/*.rb'].each {|file| require file }
 
-configure :development do
-  # %x(rake expire_cache)
-  #set :cache_enabled, false
-  set 'analytics', false
-
-  ActiveRecord::Base.establish_connection(
-    :adapter   => 'sqlite3',
-    :pool      => 25,
-    :database  => './db/devel.db'
-  )
+if $0 == __FILE__
+  SinatraWiki::App.run!
 end
-
-configure :production do
-  set 'analytics', true
-
-  db = ENV["DATABASE_URL"]
-  if db.match(/postgres:\/\/(.*):(.*)@(.*)\/(.*)/) 
-    username = $1
-    password = $2
-    hostname = $3
-    database = $4
-
-    ActiveRecord::Base.establish_connection(
-      :adapter  => 'postgresql',
-      :host     => hostname,
-      :username => username,
-      :password => password,
-      :database => database
-    )
-  end
-end
-
-before do
-  content_type 'text/html', :charset => 'utf-8'
-end
-
-get '/' do
-  @pages = Page.all 
-  erb :home
-end
-
-get '/base.css' do
-  sass :'stylesheets/base'
-end
-
-get '/:slug' do
-  @page = Page.find_by_url(params[:slug])
-  if @page.nil?
-    redirect "/#{params[:slug]}/edit"
-  else
-    erb :page
-  end
-end
-
-get '/:slug/edit' do
-  auth
-  @page = Page.find_by_url( params[:slug] )
-  @new  = @page.nil? 
-  if @new
-    @page = Page.new(
-      :url => params[:slug]
-    )
-  end
-
-  erb :edit
-end
-
-post '/:slug/?' do
-  auth
-  nice_title = Slugalizer.slugalize(params[:title])
-  @page = Page.create(
-    :url  => nice_title,
-    :body => params[:body]
-  )
-  redirect "/#{nice_title}"
-end
-
-put '/:slug/?' do
-  auth
-  nice_title = Slugalizer.slugalize(params[:title])
-  @page      = Page.find_by_url( params[:slug] )
-  @page.url  = nice_title
-  @page.body = params[:body]
-  @page.save
-  redirect "/#{nice_title}"
-end
-
 
